22492dacd1845b34b888fbb164ef7b8d868754f8
Chapita5 <132171058+Chapita5@users.noreply.github.com>
2025-11-19 20:35:25 -0400

    1er push

---
+++ src/main/java/expo4to/gestion_AD/GestionAdApplication.java
@@ -0,0 +1,13 @@
+package expo4to.gestion_AD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expo4to/gestion_AD/GestionAdApplicationTests.java
@@ -0,0 +1,13 @@
+package expo4to.gestion_AD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
